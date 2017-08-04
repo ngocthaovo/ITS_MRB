8a12bab690d84b060aa646aecab4786d7e9e7400
Kira <kira.trong@feavn.com.vn>
2017-08-04 14:11:24 +0700

    Save change 20170804 1411

---
+++ FEA_ITS_Site/Helper/MRB/FormNewAppointment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using FEA_BusinessLogic;
+namespace FEA_ITS_Site.Helper.MRB
+{
+    public class FormNewAppointment:ScheduleBase
+    {
+        public FormNewAppointment() { }
+        public int ID { get; set; }
+        [Required(ErrorMessage = "The Subject must contain at least one character.")]
+        public string Subject { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public string UserList { get; set; }
+        public string ItemList { get; set; }
+        public string ItemId { get; set; }
+    //    public string UserID { get; set; }
+
+        public FormNewAppointment(MRBBookingDetail mrb)
+            : base(mrb)
+        {
+            if (mrb != null)
+            {
+                Subject = mrb.Subject;
+                StartTime = mrb.StartTime.Value;
+                EndTime = mrb.EndTime.Value;
+                Description = mrb.Description;
+                UserList = mrb.UserList;
+                ItemList = mrb.ItemList;
+                ItemId = mrb.ItemID;
+            //    UserID = mrb.UserID;
+            }
+        }
+        public override void Assign(ScheduleBase source)
+        {
+            base.Assign(source);
+            FormNewAppointment mrbs = source as FormNewAppointment;
+            if (mrbs != null)
+            {
+                Subject = mrbs.Subject;
+                StartTime = mrbs.StartTime;
+                EndTime = mrbs.EndTime;
+                Description = mrbs.Description;
+                UserList = mrbs.UserList;
+                ItemList = mrbs.ItemList;
+                ItemId = mrbs.ItemId;
+            //    UserID = mrbs.UserID;
+            }
+        }
+    }
+}
---
+++ FEA_ITS_Site/Helper/MRB/SchedulerAgendaPersonal.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.XtraScheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FEA_BusinessLogic.MRBScheduler;
+using System.Collections;
+using FEA_BusinessLogic;
+namespace FEA_ITS_Site.Helper.MRB
+{
+    public class SchedulerAgendaPersonal
+    {
+        public bool ShowViewNavigator { get; set; }
+        public bool ShowViewVisibleInterval { get; set; }
+
+        public AppointmentStatusDisplayType AppointmentsStatus { get; set; }
+        public bool ShowResource { get; set; }
+        public bool ShowLabel { get; set; }
+        public bool ShowRecurrence { get; set; }
+        public int DayCount { get; set; }
+        
+        public SchedulerAgendaPersonal()
+        {
+            ShowViewNavigator = true;
+            ShowViewVisibleInterval = true;
+
+            AppointmentsStatus = AppointmentStatusDisplayType.Bounds;
+
+            ShowResource = true;
+            ShowLabel = true;
+            ShowRecurrence = true;
+
+            DayCount = 3;
+        }
+        public IEnumerable Appointments
+        {
+            get { return SchedulerManager.DataObject.Appointments; }
+        }
+        public IEnumerable Resources
+        {
+            get
+            {
+                return ((List<MRBBookingDetail>)SchedulerManager.DataObject.Resources).Where(x => x.ID > 3).ToList();
+            }
+        }
+    }
+}
